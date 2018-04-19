f319bf75c5fdbcf1db5aad20d42d865da75bc3ce
jash-git <jash.liao@gmail.com>
2018-04-19 16:53:01 +0800

    create

---
+++ CS_libcurl/CS_libcurl/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using SeasideResearch.LibCurlNet;  
+
+namespace CS_libcurl
+{
+    class Program
+    {
+        private static BinaryWriter bw = null;  
+        public static Int32 OnWriteData(Byte[] buf, Int32 size, Int32 nmemb,
+                                        Object extraData)
+        {
+            Program.bw.Write(buf);
+            return size * nmemb;
+        } 
+        static void Main(string[] args)
+        {
+            try
+            {
+                Curl.GlobalInit((int)CURLinitFlag.CURL_GLOBAL_ALL);
+
+                Easy easy = new Easy();
+                Easy.WriteFunction wf = new Easy.WriteFunction(OnWriteData);
+
+                if (args.Length > 1)
+                {
+                    easy.SetOpt(CURLoption.CURLOPT_URL, args[0]);
+                }
+                else
+                {
+                    string url = string.Empty;
+                    Console.WriteLine("Download URL: ");
+                    url = "http://jashliao.pixnet.net/blog";//Console.ReadLine();
+
+                    easy.SetOpt(CURLoption.CURLOPT_URL, url);
+                }
+                easy.SetOpt(CURLoption.CURLOPT_VERBOSE, 1);
+                easy.SetOpt(CURLoption.CURLOPT_WRITEFUNCTION, wf);
+                easy.SetOpt(CURLoption.CURLOPT_NOPROGRESS, 1);
+
+                string path = string.Empty;
+                Console.WriteLine("Save Path: ");
+                path = "123.html";//Console.ReadLine();
+                Program.bw = new BinaryWriter(new FileStream(path, FileMode.Create));
+
+                easy.Perform();
+
+                easy.Cleanup();
+
+                Program.bw.Close();
+
+                Curl.GlobalCleanup();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }  
+        }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,5 @@
+﻿C# 呼叫 libcurl 函式庫(CS libcurl)
+
+基本測試整合LIB範例: https://blog.csdn.net/pengqianhe/article/details/7965654
+C#函式庫: https://sourceforge.net/projects/libcurl-net/files/?source=navbar
+純C範例網站: https://curl.haxx.se/libcurl/c/example.html
